b069eb9cf3cd054f76a40250ebcb1fea9d87982b
Rick_Sanchez <constantin.pavlovic@gmail.com>
2017-06-02 12:29:54 +0200

    Update Display.php

--- app/controllers/Display.php
+++ app/controllers/Display.php
@@ -35,50 +35,25 @@ class Display extends ControllerBase
 	{
 		
 		$vHost=DAO::getOne("models\Virtualhost","id=".$idvirtualhost);		
-		$vHostProperty=DAO::getOne("models\Virtualhostproperty","idVirtualhost=".$idvirtualhost);
+		$vHost_Propertys=DAO::getALL("models\Virtualhostproperty","idVirtualhost=".$idvirtualhost);
+		
 		
 		
 		$id_host=1; /*FAUSSE VARIABLE EN ATTENDANT DE RECUPER LA VRAIE*/
 		
-		if ( isset($vHostProperty) ) /*Test de la présence ou non de properties sur le vhost*/
-		{
-
-			$vhP_value = "";
-			$vhP_active = "";  
-			$vhP_comment = "";
-			
-			
-			
-			$vHost_prop_value=DAO::getAll("models\VirtualhostProperty","idvirtualhost=".$idvirtualhost); /*Creation tableau contenant les valeures des propriétes d'un vhost sous forme d'objet*/
-			$property=DAO::getAll("models\Property"); /*Creation tableau contenant les propriétes d'un vhost sous forme d'objet*/
-			
-			$vHost_prop = 
-			[
-					$property[0]->__toString()=>$vHost_prop_value[0]->__toString(),
-					$property[1]->__toString()=>$vHost_prop_value[1]->__toString(), /*Creation du tableau + transformation des objets en chaines de caractères exploitables*/
-					$property[2]->__toString()=>$vHost_prop_value[2]->__toString(), 
-					$property[3]->__toString()=>$vHost_prop_value[3]->__toString()
-			];
-
-		}
-		else 
-		{
-			$vhP_value = "N/A";
-			$vhP_active = 0;			/*Déclaration de variables vides à afficher si le vhost n'a pas de properties*/
-			$vHost_prop = "";
-		}
-		
 		
 		$this->jquery->compile($this->view);
 		
+		$this->jquery->exec("Prism.highlightAll();",true);
+		
 		$this->loadView("Display/virtualhost.html"
 		,array(
-			"id_vhost"=>$idvirtualhost,"nom_vhost"=>$vHost->getName(),"config_vhost"=>$vHost->getConfig(),"id_serveur_vhost"=>$vHost->getServer() /* Conf obligatoire d'un vhost */
-			,"vhP_value"=>$vhP_value,"vhP_active"=>$vhP_active /* Seulement utile si le vhost ne possède pas de properties*/
-			,"vHost_prop"=>$vHost_prop /* Passage à la vue du tableau associant propiété=>valeur */
+			"vHost"=>$vHost
 			,"id_host"=>$id_host /*recuperer l'IDhost pour rediriger correctement l'utilisateur*/
+			,"vhps"=>$vHost_Propertys 
 		)); 
 		
+			
 	}
 	
 	

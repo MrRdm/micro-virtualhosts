264905e4c1999f200ad631e3f1824f641982e40e
MARTIN Brandon <martin01.brandon@outlook.com>
2017-06-13 10:15:36 +0200

    Add files via upload

--- app/controllers/My.php
+++ app/controllers/My.php
@@ -20,7 +20,7 @@ class My extends ControllerBase{
 			$user=Auth::getUser();
 			$hosts=DAO::getAll("models\Host","idUser=".$user->getId());
 			
-	
+	//permet d'affiche, obligé de l'appeler dans twig'
 			$hostsItems=$this->semantic->htmlItems("list-hosts");
 			$hostsItems->fromDatabaseObjects($hosts, function($host){
 				$item=new HtmlItem("");
@@ -32,40 +32,49 @@ class My extends ControllerBase{
 			});
 
 			
-			
-			
-				//A faire : ajouter virtualhosts
+	//TODO1
+		
 				//Acceder à tous les vhosts en fonction de l'userID passé en paramètres
 				//utilise un des models pour acceder à un des virtuals hosts 
+				//met das un variable tous les vhosts grâce à la fonction memebre presente dans le modèle virtualhost.php qui lui même va les chercher dans la database
+				// mais y a un paramètre idUser, que l'on applicuqe getID, pour avoir seulement ID du user (on choppe une donné de l'onjet user')
 				$vhosts=DAO::getAll("models\Virtualhost","idUser=".$user->getId());
 				
-				//Acceder au server des vhosts [NON FONCTIONNEL]
-				//$vhosts_server=$vhosts->getServer();
 
-				//Permet de compiler
+				//Permet de compiler en Java
 				$this->jquery->compile($this->view);
 
 				//Permet de charger dans la vue les variables instanciées dans le contrôleur(=cette page)
 				$this->loadView("My/index.html"
 						,array(
-						"tableau_vhosts"=>$vhosts,
-						"tableau_server"=>$vhosts_server
+						"tableau_vhosts"=>$vhosts
+						//"tableau_server"=>$vhosts_server
 
 
 				) );
 			
 		}
 		
-		
+	//TODO4
+
 		/* PROTECTION DU CONTROLLEUR MY */
 		
 		else 
 		{
+			//Utilisation de PHP-MVUI
+			//Pris dans le contrôler login
+			//pas obligé twing parce qu'on l'appel directement ici avec echo $message
+			//Créer un objet "message" et on lui applique la méthode semantic (pour dire utilise semanticui) et html message (permet de mettre "merci de vous connecter pour tester") 
 			$message=$this->semantic->htmlMessage("error","Merci de vous connecter pour tester.");
+			//rejoute l'icone au message affiché précedament avec la méthode setIcon créer par le grand dieu'
 			$message->setIcon("announcement")->setError();
+			//Ajouter la croix en haut à gauche pour fermer la mesage percedement affiché
 			$message->setDismissable();
-			$message->addContent(Auth::getInfoUser($this,"-login")); //Changé le Login:: en Auth::
+			//permet d'ajouter le bouton pour le test, on a changé la librairie Login:: en Auth::
+			$message->addContent(Auth::getInfoUser($this,"-login"));
+			//Affichage de l'objet $message
 			echo $message;
+			//Utilisation de Java pour pouvoir cliquer sur le bouton pour s'identifier'
 			echo $this->jquery->compile($this->view);
 		}
 		
